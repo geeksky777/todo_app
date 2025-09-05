0a4d1906a5306b1305fdad17aca31e3b40b9a158
geeksky777 <nenabe9620@efpaper.com>
2025-09-05 16:55:42 +0300

    add alembic + 1st revision

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(".env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
--- app/models/task.py
+++ app/models/task.py
@@ -1,11 +1,11 @@
-from sqlalchemy import ForeignKey, String, Text
-from db.base import Base, TimestampMixin
+from sqlalchemy import ForeignKey, String, Text, Enum
+from app.db.base import Base, TimestampMixin
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Enum
+import enum
 from sqlalchemy.orm import relationship
 
 
-class Status(Enum):
+class Status(enum.Enum):
     compiled = "compiled"
     not_completed = "not_completed"
 
@@ -16,7 +16,10 @@ class Task(Base, TimestampMixin):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(Text)
-    status: Mapped[Status] = mapped_column(Enum(Status), default=Status.not_completed)
+    status: Mapped[Status] = mapped_column(
+        Enum(Status, name="status_enum"),
+        default=Status.not_completed,
+    )
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
 
     user = relationship("User", back_populates="tasks")
