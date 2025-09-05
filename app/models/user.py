266d67649636bc6c7d1283be9572e9e57f6c2756
geeksky777 <nenabe9620@efpaper.com>
2025-09-05 09:15:55 +0300

    add model user

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+import datetime
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
